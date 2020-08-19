5863f708dbec7559ee013baa1e6943a4e5847469
jack01-make <61861432+jack01-make@users.noreply.github.com>
2020-08-19 19:13:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy-git-master.jack01-make.vercel.app',
+    serverName: 'siteproxy007.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
